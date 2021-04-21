b6f7ad22480be04d774e66617b8fbb48237f770e
강원기(KangWonKi) <78646944+KangCine@users.noreply.github.com>
2021-04-22 00:15:43 +0900

    20210224 Servlet을 이용한 MVC pattern 게시판

---
+++ Workspaces05_Web/Jsp03_MVC_Servlet/src/com/mvc/dao/MVCBoardDao.java
@@ -0,0 +1,27 @@
+package com.mvc.dao;
+
+import java.util.List;
+
+import com.mvc.dto.MVCBoardDto;
+
+public interface MVCBoardDao {
+	
+	
+	public String SELECT_LIST_SQL = " SELECT SEQ, WRITER, TITLE, CONTENT, REGDATE FROM MVCBOARD ORDER BY SEQ DESC ";
+	public String SELECT_ONE_SQL = " SELECT SEQ, WRITER, TITLE, CONTENT, REGDATE FROM MVCBOARD WHERE SEQ = ? ";
+	public String INSERT_SQL = " INSERT INTO MVCBOARD VALUES(MVCBOARDSEQ.NEXTVAL, ?, ?, ?, SYSDATE)  ";
+	public String UPDATE_SQL = " UPDATE MVCBOARD SET TITLE = ?, CONTENT = ? WHERE SEQ = ? ";
+	public String DELETE_SQL = " DELETE FROM MVCBOARD WHERE SEQ = ? ";
+	
+	
+	public List<MVCBoardDto> selectlist();
+	public MVCBoardDto selectOne(int seq);
+	public int insert(MVCBoardDto dto);
+	public int update(MVCBoardDto dto);
+	public int delete(int seq);
+	public int multiDelete(String[] seqs);
+	
+	
+	
+
+}

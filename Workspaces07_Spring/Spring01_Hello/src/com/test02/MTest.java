98e8515f3e70f238ee8821f2b9ab022236632a9a
강원기(KangWonKi) <78646944+KangCine@users.noreply.github.com>
2021-04-24 00:02:05 +0900

    20210413 Hello, Spring!

---
+++ Workspaces07_Spring/Spring01_Hello/src/com/test02/MTest.java
@@ -0,0 +1,14 @@
+package com.test02;
+
+public class MTest {
+	
+	public static void main(String[] args) {
+		MessageBean korean = new MessageBeanKo();
+		korean.sayHello("스프링");
+		
+		MessageBean english = new MessageBeanEn();
+		english.sayHello("Spring");
+		
+	}
+	
+}

42513da4e8a408331a6c1845b923fea052547bd1
KangCine <kwg940930@gmail.com>
2021-05-10 15:34:23 +0900

    20210510 python 문법 if, for, while, break, continue

---
+++ Workspaces09_Python/python01/com/test01/control02_for.py
@@ -0,0 +1,30 @@
+subject = ['java', 'javascript', 'python']
+
+for i in subject:
+    # print(i)
+    print(i, end=' ')
+else:
+    # 반복문이 정상 종료되었을 때 실행
+    print('재밌다.')
+
+for i in range(1, 100):
+    print(i, end=' ')
+print()
+
+for i in range(10, 0, -1):
+    print(i, end=' ')
+print()
+
+for i in range(1, 10, 2):
+    print(i, end=' ')
+print()
+
+print('----------')
+print('<구구단>')
+for i in range(2, 10):
+    print('<<'+str(i)+'단>>')
+    for j in range(1, 10):
+        # print(str(i) + ' * ' + str(j) + ' = ' + str(i*j))
+        print(i, '*', j, '=', i*j, sep=' ')
+    print()
+

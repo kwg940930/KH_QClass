3a7454527e85efad35ba55ea8f078cd86f4b788b
KangCine <kwg940930@gmail.com>
2021-05-13 15:22:49 +0900

    20210513 python web

---
+++ Workspaces09_Python/Python03/com/hello/hello01.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# pip install flask
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "hello, world"
+
+
+if __name__ == '__main__':
+    app.run()
